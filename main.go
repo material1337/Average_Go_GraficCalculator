83c764024c20b98b38bae6b95f0b9f86eade78f0
material1337 <mamkin1dota1reper1337@gmail.com>
2025-05-19 22:40:55 +0300

    Добавил кнопку, но сырую

--- main.go
+++ main.go
@@ -1,20 +1,28 @@
 package main
 
 import (
+	"fmt"
 	//"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
 )
 
 func main() {
 	a := app.New()
 	window := a.NewWindow("Calculator")
-	window.Resize(fyne.NewSize(600, 500))
+	window.Resize(fyne.NewSize(500, 600))
 
 	label1 := widget.NewLabel("")
-	label1.Resize(fyne.NewSize(600, 100))
+	label1.Resize(fyne.NewSize(500, 100))
 
-	//window.SetContent(Content)
+	button1 := widget.NewButton("ONE", func() {
+		label1.SetText(fmt.Sprintf("1"))
+	})
+	button1.Resize(fyne.NewSize(100, 100))
+
+	content := container.NewVBox(label1, button1)
+	window.SetContent(content)
 	window.ShowAndRun()
 }
